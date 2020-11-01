e574fbc7e3e5f9afd82a0f5a780e87890f59b712
JulietteM81 <juliettemerle2010@gmail.com>
2020-11-01 14:47:15 +0100

    private files

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const cors  = require('cors')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+const config = require('./config/key')
+const { User } = require('./models/user')
 
 require('dotenv').config();
 
@@ -9,7 +14,7 @@ require('dotenv').config();
 const db = async () => {
     try {
         //mongoose= need the url where you wanna connect then config options
-      const success = await mongoose.connect(process.env.MONGODB_CLOUD, {
+      const success = await mongoose.connect(config.mongoURI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
@@ -25,12 +30,26 @@ db();
 
 //middleswares
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
 
 app.get('/', (req, res) => {
-    res.send('hello')
+    res.send('Hello you ')
+})
+
+app.post('/api/users/register', (req, res) => {
+    //after reading that request ==> need to send info in mongodb 
+    const user = new User(req.body)
+    user.save((err, userData) => {
+        if(err) return res.json({ success: false, err})
+        //if we don't have any errors
+        return res.status(200).json({ success : true })
+    })
 })
 
 //port ==> access .env file/variables through process.env.variable
-app.listen(process.env.PORT, ()=> {
+app.listen(config.PORT, ()=> {
     console.log(`Server is running port : ${process.env.PORT}`);
 })
